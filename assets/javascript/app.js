b3eb7f04fb5dabbcd8af2bc66ebdf726f79a3123
Kchar <38297595+kevinlichang@users.noreplay.github.com>
2018-09-20 22:22:58 -0400

    added quiz and results container and functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,118 @@
-// variables for number of correct and wrong answers
-var correctCount = 0;
-var incorrectCount = 0;
-var unanswered = 0;
-
-
-// function to show questions on button start click
-function showQuiz() {
-  // hide start screen
-  $("#start-screen").attr("style", "display: none")
-
-
-  // show the questions and answer choices
+$(document).ready(function () {
+  // variables for number of correct and wrong answers
+  var correctCount = 0;
+  var incorrectCount = 0;
+  var unanswered = 0;
 
 
 
-  for (var i = 0; i < questionSet.length; i++) {
-    $("#quiz-screen").append("<div>" + questionSet[i].question + "</div>");
+  function displayQuestions() {
+
+    for (var i = 0; i < questionSet.length; i++) {
+      // variables to create div. add text and append to html
+      var questionDiv = $("<div>");
+      questionDiv.text(questionSet[i].question);
+      $("#quiz-questions").append(questionDiv);
+
+      // function to create radio inputs and labels for multiple choice
+      // add attribute type=radio, name, and value as the answer.
+      function addInput(data) {
+        var choicesInput = $("<input>");
+        choicesInput.attr("type", "radio");
+        choicesInput.attr("name", questionSet[i].questNumber);
+        choicesInput.attr("value", data);
+        $("#quiz-questions").append(choicesInput);
+
+        var choicesLabel = $("<label>");
+
+        choicesLabel.text(data);
+        $("#quiz-questions").append(choicesLabel);
+
+      };
+
+      // Run addInput function to add 4 multiple choices for each question
+      addInput(questionSet[i].choicesArray[0]);
+      addInput(questionSet[i].choicesArray[1]);
+      addInput(questionSet[i].choicesArray[2]);
+      addInput(questionSet[i].choicesArray[3]);
+
+    };
+
+  };
+
+
+
+
+
+
+  // function to show questions on button start click
+  $("#start-button").click(function () {
+    // hide start screen
+    $("#start-screen").attr("style", "display: none");
+
+    // show #quiz-container
+    $("#quiz-container").removeAttr("style");
+
+    // show questions and choices
+    displayQuestions();
+
+
+
+  });
+
+
+
+
+  // function to create timer
+
+
+
+  // function to check answers and show results 
+
+  function checkAnswers() {
+    //created variables to acquire value of checked radio inputs
+    var checkedAnsValues = [
+      $('input[name="0"]:checked').val(),
+      $('input[name="1"]:checked').val(),
+      $('input[name="2"]:checked').val(),
+      $('input[name="3"]:checked').val()
+    ];
+    console.log(checkedAnsValues);
+
+
+    // checked the checked answer value with the answer in questionSet array
+    // increase correctCount and incorrect Count based on comparison
+
+    for (var i = 0; i < questionSet.length; i++) {
+      if (checkedAnsValues[i] === questionSet[i].answer) {
+        correctCount++;
+        
+      } else if (checkedAnsValues[i] !== questionSet[i].answer) {
+        incorrectCount++;
+      }
+    };
+    console.log("Correct: " + correctCount);
+    console.log("Incorrect: " + incorrectCount);
+    $("#correctAns").text("Questions you got correct: " + correctCount);
+    $("#wrongAns").text("Questions you got wrong: " + incorrectCount);
+  };
+
+  $("#finished-button").click(function () {
+    // hide quiz container
+    $("#quiz-container").attr("style", "display: none");
+    $("#results-container").removeAttr("style");
+
+    checkAnswers();
+
     
-    
-
-  }
 
 
 
 
-
-}
-
-
-$("#start-button").click()
-
-
-// function to create timer
+  });
+  // click function for submit button
 
 
 
-// function to check answers and show results 
-
-
-// function to reset
+  // function to reset
+});
--- assets/javascript/questionSet.js
+++ assets/javascript/questionSet.js
@@ -1,17 +1,23 @@
 var questionSet = [{
   question: "What was the first movie released in the Marvel Cinematic Universe?",
-  answer: "Iron Man"
+  questNumber: 0,
+  answer: "Iron Man",
+  choicesArray: ["Incredible Hulk", "Captain America", "Spiderman", "Iron Man"]
 }, {
   question: "Who starred as a genius character in Good Will Hunting?",
-  answer: "Matt Damon"
+  questNumber: 1,
+  answer: "Matt Damon",
+  choicesArray: ["Matt Damon", "Ben Affleck", "Robin Williams", "Kevin Costner"]
 }, {
   question: "Which movie was also directed by the director of The Dark Knight?",
-  answer: "Memento"
+  questNumber: 2,
+  answer: "Memento",
+  choicesArray: ["Donnie Darko", "Memento", "The Illusionist", "Gravity"]
+}, {
+  question: "Which movie was not based on a true story",
+  questNumber: 3,
+  answer: "Sicario",
+  choicesArray: ["Spotlight", "American Hustle", "Zodiac", "Sicario"]
 }
 ];
 
-var answerChoices [
-  ["Incredible Hulk", "Captain America", "Spiderman", "Iron Man"],
-  ["Matt Damon", "Ben Affleck", "Robin Williams", "Kevin Costner"],
-  ["Donnie Darko", "Memento", "The Illusionist", "Gravity"]
-]
